00292ceac5c4713e80bce3eaf7e9f39b1cbbacd6
blancojohn <johnmario.bdc@gmail.com>
2024-06-06 18:29:36 -0400

    Creación base datos: migraciones de tablas OK

--- src/app.py
+++ src/app.py
@@ -1,27 +1,29 @@
-import os
+#Archivo principal en donde se parametrizan las cosas que se van instalando
+import os #permite interacturar con el sistema operativo indistantemente en el que se encuentre para las variables de entorno
 from flask import Flask, jsonify
-from flask_migrate import Migrate
-from flask_cors import CORS
-from dotenv import load_dotenv
-from models import db
+from flask_migrate import Migrate #Libreria que da acceso a comandos para poder poblar la base de datos con las tablas, colummnas, datos.
+from flask_cors import CORS #Permite establecer conexión con el front para hacer consultas con fetch. 
+from dotenv import load_dotenv #Permite cargar en memoria la info del archivo .env.
+from models import db #Permite vincular a través de migrate a app.py con models.py y los comandos de migrate.
 from routes import api
 
 load_dotenv()
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__) #Instanacia de la aplicación Flask. Name hace referencia al mismo archivo.
+#A continuación connfiguración de variables por medio de la propiedad config:
+app.config['DEBUG'] = True #Permite saber si hay errores en modo desarrolo en true.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #En false permite que los cambios se han visibles pero cuando se generen las migraciones.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
-db.init_app(app)
-Migrate(app, db) # db init, db migrate, db upgrade, db downgrade
+db.init_app(app) # víncula el archivo models con la aplicación Flask
+Migrate(app, db) # da accesso a los comandos db init, db migrate, db upgrade, db downgrade
 CORS(app)
 
-@app.route('/')
+@app.route('/') #Ruta principal
 def main():
     return jsonify({ "status": "App Running Successfully!"}), 200
 
 app.register_blueprint(api, url_prefix='/api')
 
-if __name__ == '__main__':
+if __name__ == '__main__': #condiciona si name es el archivo principal inicia app
     app.run()
--- src/models.py
+++ src/models.py
@@ -1,14 +1,78 @@
+#En este archivo se crean las tablas que van a estar en la base de datos
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+db = SQLAlchemy()    #Inicializa el archivo models para interactuar con la base de datos y crear 
+                     #las tablas dentro de ellas.
 
-class User(db.Model):
+class Planet(db.Model): #Contiene los planetas de StarWars.
+    __tablename__= 'planets'
+    uid= db.Column(db.Integer, primary_key= True)
+    name= db.Column(db.String(200), unique= True, nullable= False)
+    url= db.Column(db.String(300), unique= True)
+    favorites_p= db.relationship('Favorite_Planet', backref= 'planet')
+
+    def to_dict(self):
+        return{
+            "uid": self.uid,
+            "name": self.name,
+            "url": self.url
+        }
+
+class Favorite_Planet(db.Model):
+    __tablename__= 'favorites_planets'
+    uid= db.Column(db.Integer, primary_key= True)
+    favorite_planet_name= db.Column(db.String(600), unique= True)
+    user_uid= db.Column(db.Integer, db.ForeignKey('users.uid'))
+    planet_uid= db.column(db.Integer, db.ForeignKey('planets.uid'))
+
+    def to_dict(self):
+        return{
+            "uid":self.uid,
+            "favorite_planet_name": self. favorite_planet_name,
+            "user_uid": self.user_uid,
+            "planet_uid": self.planet_uid
+        }
+
+
+class People(db.Model): #Contiene los personajes de StarWars.
+    __tablename__= 'characters'
+    uid= db.Column(db.Integer, primary_key= True)
+    name= db.Column(db.String(200), unique= True, nullable= False)
+    url= db.Column(db.String(300), unique= True,)
+    favorites_c= db.relationship('Favorite_People', backref='people')#Un personaje puede ser favorito de mucho usuarios
+
+    def to_dict(self):  #CONVIERTE EL OBJETO CLASE EN UN DICCIONARIO#
+        return{
+            "uid": self.uid,
+            "name": self.name,
+            "url": self.url
+        }
+        
+class Favorite_People(db.Model): #Contiene los personajes favoritos seleccionados por los usuarios
+    __tablename__= 'favorites characters'
+    uid= db.Column(db.Integer, primary_key= True)
+    favorite_people_name= db.Column(db.String(600), unique= True)
+    user_uid= db.Column(db.Integer, db.ForeignKey('users.uid'))
+    people_uid= db.Column(db.Integer, db.ForeignKey('characters.uid'))
+
+    def to_dict(self):
+        return{
+            "uid": self.uid,
+            "favorite_people_name": self.favorite_people_name,
+            "user_uid": self.user_uid,
+            "people_uid": self.people_uid
+        }
+
+class User(db.Model): #Contiene a los usuarios que se agregan al block
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
-    
-    def serialize(self):
+    uid= db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String(120), nullable=False, unique=True)
+    favorites_characters= db.relationship('Favorite_People', backref= 'user')
+    favorites_characters= db.relationship('Favorite_Planet', backref= 'user')
+    #A FUTURO SE AGREGARÁ EMAIL Y PASSWORD PARA IMPLEMENTAR JWT.
+
+    def to_dict(self):
         return {
-            "id": self.id,
+            "id": self.uid,
             "username": self.username
         }
+   
